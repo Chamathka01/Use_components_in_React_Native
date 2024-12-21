5887666bd6bd934fd3609ed605bca1ec290ea978
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-12-21 11:22:03 +0000

    Used styles for body

--- myapp/MyMobileApp/App.js
+++ myapp/MyMobileApp/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <PaperProvider>
       <View style={styles.container}>
       <Text variant="headlineLarge">Headline Large</Text>
-      <Text variant="bodyMedium">
+      <Text variant="bodyMedium" style={styles.body}>
       Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document.
       To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries.
       Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme.
@@ -28,4 +28,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  body:{
+    padding:8,
+    textAlign:'justify'
+  }
 });
