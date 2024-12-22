b6d21b6d9e629be3bcb5d7ef1a918935230f5750
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2024-12-22 13:54:09 +0000

    Added heading and divider for ContactUs.js

--- myapp/MyMobileApp/components/ContactUs.js
+++ myapp/MyMobileApp/components/ContactUs.js
@@ -1,9 +1,12 @@
 import { StyleSheet } from 'react-native';
+import { Text, Divider, Button } from 'react-native-paper';
 
 export default function ContactUs(){
     return(
         <>
         <View style={styles.header}>
+        <Text variant="headlineLarge">Contact Us</Text>
+        <Divider/>
         </View>
         <View style={styles.body}>
         </View>
